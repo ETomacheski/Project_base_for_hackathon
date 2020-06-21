2a518fd965ddf8ebfb4e73ff5825b8cdc148a84a
ETomacheski <tomacheski2001@gmail.com>
2020-06-20 22:30:18 -0300

    configurando eslint

--- src/server.js
+++
@@ -1,6 +0,0 @@
-var express_1 = require('express');
-var app = express_1["default"]();
-app.get('/', function (request, response) {
-    return response.json({ message: 'Hello Word' });
-});
-app.listen(3333);
